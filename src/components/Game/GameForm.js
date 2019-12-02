92aab30596d99b8e74a98687146252e92b0e50d4
HajimeNoJosh <joshuah.m.martin@gmail.com>
2019-12-02 16:35:23 -0500

    Begin work on updating coords
    1. Create a GameForm so we can input the coords
    2. Create axios calls to PATCH information
    3. Redirect as needed

---
+++ src/components/Game/GameForm.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+const GameForm = (props) => {
+  const { game, handleChange, handleSubmit, cancelPath } = props
+  return (
+    <div className="row">
+      <div className="col-sm-10 col-md-8 mx-auto mt-5">
+        <h2>Add A Move</h2>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="title">
+            <Form.Label>Coordinate</Form.Label>
+            <Form.Control
+              placeholder="Enter your move"
+              value={game.coords}
+              name="coords"
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Button type="submit" variant="primary">Submit</Button>
+          <Button type="button" href={cancelPath} variant="secondary" className="ml-2">Cancel</Button>
+        </Form>
+      </div>
+    </div>
+  )
+}
+
+export default GameForm
--- src/components/Game/MyGame.js
+++ src/components/Game/MyGame.js
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import apiUrl from '../../apiConfig'
 import { withRouter } from 'react-router-dom'
+import GameForm from './GameForm.js'
 
 const MyGame = (props) => {
-  console.log(props)
-  const [game, setGame] = useState(null)
+  // const [updated, setUpdated] = useState(false)
+  const [game, setGame] = useState({ coords: [] })
   useEffect(() => {
     axios({
       url: `${apiUrl}/games/${props.match.params.id}`,
@@ -21,13 +22,43 @@ const MyGame = (props) => {
       .catch(console.error)
   }, [])
 
+  const handleChange = event => {
+    event.persist()
+    console.log(event.target.value)
+    setGame(game => ({ ...game, coords: [event.target.value] }))
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    axios({
+      url: `${apiUrl}/games/${props.match.params.id}`,
+      method: 'PATCH',
+      headers: {
+        'Authorization': `Bearer ${props.user.token}`
+      },
+      data: { game }
+    })
+      .then(response => {
+        props.alert({ heading: 'Success', message: 'You updated a game', variant: 'success' })
+      })
+      .catch(() => props.alert({ heading: 'Nah...', message: 'That didn\'t work', variant: 'danger' }))
+  }
+
   if (!game) {
     return <p>Loading...</p>
   }
+  console.log(game)
 
   return (
     <div>
-      <h1>{game.coords}</h1>
+      <GameForm
+        game={game}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        cancelPath={`#games/${props.match.params.id}`}
+      />
+      {game.coords}
     </div>
   )
 }
