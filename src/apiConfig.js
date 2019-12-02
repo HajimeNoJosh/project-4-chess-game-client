103c45b9ecfa371562c7e50b8e12625804c1dc0a
HajimeNoJosh <joshuah.m.martin@gmail.com>
2019-12-02 09:42:00 -0500

    initial commit

---
+++ src/apiConfig.js
@@ -0,0 +1,13 @@
+let apiUrl
+const apiUrls = {
+  production: 'http://stark-citadel-18124.herokuapp.com/',
+  development: 'http://localhost:4741'
+}
+
+if (window.location.hostname === 'localhost') {
+  apiUrl = apiUrls.development
+} else {
+  apiUrl = apiUrls.production
+}
+
+export default apiUrl
