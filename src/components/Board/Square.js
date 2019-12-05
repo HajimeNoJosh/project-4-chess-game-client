1313a95eeff1d317cf32196f4dbfb7ebcc6a30e5
HajimeNoJosh <joshuah.m.martin@gmail.com>
2019-12-04 20:08:07 -0500

    Make images

--- src/components/Board/Square.js
+++ src/components/Board/Square.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Square = (props) => {
+  console.log(props.text)
   return (
     <div
-      className={'square ' + props.className + ' ' + props.text}
+      className={'square ' + props.className + ' ' + props.text + ' piece'}
       id={props.id}
       text={props.text}
       onClick={props.onClick}>
-      {props.text}
     </div>
   )
 }
